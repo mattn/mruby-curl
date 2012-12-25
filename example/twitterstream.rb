171851eff5b5971e2f9a90cc98b0b63d487d27aa
mattn <mattn.jp@gmail.com>
2012-12-25 22:19:03 +0900

    check the tweet is text.

--- example/twitterstream.rb
+++ example/twitterstream.rb
@@ -7,7 +7,8 @@ end
 Curl::SSL_VERIFYPEER = 0
 Curl::get("https://stream.twitter.com/1.1/statuses/sample.json", {"Authorization"=> "Basic #{Base64::encode(ARGV[0] + ":" + ARGV[1])}"}) do |h,b|
   begin
-    puts JSON::parse(b)['text']
+    tweet = JSON::parse(b)
+    puts "#{tweet['user']['screen_name']}: #{tweet['text']}" if tweet.has_key?('text')
   rescue RuntimeError
   end
 end
