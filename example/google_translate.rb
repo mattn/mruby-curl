8009a01de54a593498945b81e364e871e5e69a35
mattn <mattn.jp@gmail.com>
2013-01-08 18:18:20 +0900

    add example for google search.

---
+++ example/google_translate.rb
@@ -0,0 +1,6 @@
+#!mruby
+
+url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=mruby&rsz=large"
+for result in JSON::parse(Curl::get(url).body)['responseData']['results']
+	puts "#{result['titleNoFormatting']}\n  #{result['url']}"
+end
