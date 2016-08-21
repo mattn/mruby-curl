b9cf68857d3fdbf3435ce3650d441b68e4ea7f22
Eric Hodel <drbrain@segment7.net>
2016-08-21 15:17:14 -0700

    Reuse curl instance for class methods
    This gives all API methods equivalent speed benefits.

--- mrblib/curl.rb
+++ mrblib/curl.rb
@@ -1,25 +1,31 @@
 class Curl
   def self.delete(url, headers = nil, &block)
-    new.delete url, headers, &block
+    @curl ||= new
+    @curl.delete url, headers, &block
   end
 
   def self.get(url, headers = nil, &block)
-    new.get url, headers, &block
+    @curl ||= new
+    @curl.get url, headers, &block
   end
 
   def self.patch(url, data, headers = nil, &block)
-    new.patch url, data, headers, &block
+    @curl ||= new
+    @curl.patch url, data, headers, &block
   end
 
   def self.post(url, data, headers = nil, &block)
-    new.post url, data, headers, &block
+    @curl ||= new
+    @curl.post url, data, headers, &block
   end
 
   def self.put(url, data, headers = nil, &block)
-    new.put url, data, headers, &block
+    @curl ||= new
+    @curl.put url, data, headers, &block
   end
 
   def self.send(url, req, headers = nil, &block)
-    new.send url, req, headers, &block
+    @curl ||= new
+    @curl.send url, req, headers, &block
   end
 end
